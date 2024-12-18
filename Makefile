143d1833c81556033878c590cfe0991e307495d6
nmlpx09 <lmpx09@lmpx.lmpx>
2024-12-18 11:27:07 +0300

    remove alsa print error

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC = clang++-19
 CCFLAGS = -O3 -std=c++23 -pedantic -Wall -W -Werror -Wextra -c -I.
+DEFINES =
 LDFLAGS = -lasound
 OBJ = bin/main.o rtp/rtp.o rcv/socket.o send/alsa.o
 RUN = bin/stream
@@ -8,9 +9,10 @@ all: $(OBJ)
 	$(CC) $^ -o $(RUN) $(LDFLAGS)
 
 %.o: %.cpp
-	$(CC) $(CCFLAGS) $< -o $@
+	$(CC) $(DEFINES) $(CCFLAGS) $< -o $@
 
 install:
+	strip $(RUN)
 	cp $(RUN) /usr/bin/
 
 clean:
--- send/alsa.cpp
+++ send/alsa.cpp
@@ -1,8 +1,6 @@
 #include "alsa.h"
 #include "errors.h"
 
-#include <iostream>
-
 namespace NSend {
 
 TAlsa::TAlsa(
@@ -37,24 +35,20 @@ TAlsa::~TAlsa() {
 
 std::error_code TAlsa::Init() noexcept {
     if (auto err = snd_pcm_open(&SoundDevice, Device.c_str(), SND_PCM_STREAM_PLAYBACK, 0); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::DeviceOpen);
     }
 
     snd_pcm_hw_params_alloca(&HwParams);
  
     if (auto err = snd_pcm_hw_params_any(SoundDevice, HwParams); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::InitializeParameter);
     }
 
     if (auto err = snd_pcm_hw_params_set_rate_resample(SoundDevice, HwParams, 1); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::Resampling);
     }
 
     if (auto err = snd_pcm_hw_params_set_access(SoundDevice, HwParams, SND_PCM_ACCESS_RW_INTERLEAVED); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetAccess);
     }
 
@@ -63,7 +57,6 @@ std::error_code TAlsa::Init() noexcept {
     }
 
     if (auto err = snd_pcm_hw_params_set_channels(SoundDevice, HwParams, Channels); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetChannels);
     }
 
@@ -76,7 +69,6 @@ std::error_code TAlsa::Init() noexcept {
     }
 
     if (auto err = snd_pcm_hw_params_set_format(SoundDevice, HwParams, format); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetFormat);
     }
 
@@ -86,23 +78,19 @@ std::error_code TAlsa::Init() noexcept {
 
     std::uint32_t rate = Rate;
     if (auto err = snd_pcm_hw_params_set_rate_near(SoundDevice, HwParams, &rate, 0); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetRate);
     }
 
     snd_pcm_uframes_t bufferSize = BufferSize;
     if (auto err = snd_pcm_hw_params_set_buffer_size_near(SoundDevice, HwParams, &bufferSize); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetBuffer);
     }
 
     if (auto err = snd_pcm_hw_params(SoundDevice, HwParams); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::SetHwParams);
     }
 
     if (auto err = snd_pcm_prepare(SoundDevice); err < 0) {
-        std::cerr << snd_strerror(err) << std::endl;
         return make_error_code(EErrorCode::Prepare);
     }
 
